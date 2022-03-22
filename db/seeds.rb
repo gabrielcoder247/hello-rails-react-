3f348b278c40f2ac2d50a9ad6cd9620c8d784c23
gabrielcoder247 <gabrielcoder247@gmail.com>
2022-03-22 11:00:51 +0100

    Add rails-react-hello

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+
+Message.create(content: 'Hello Everyone')
+Message.create(content: 'Good morning')
+Message.create(content: 'Good afternoon ')
+Message.create(content: 'Good  Everning')
+Message.create(content: 'Hello world ')
